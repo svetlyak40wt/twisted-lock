e595a8e24ea5027e61d90a6abe9fc4446eecae8e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-10-16 13:31:01 +0400

    Полуработающие выборы, нехватает передачи информации о мастере вновь подключенному узлу.

--- lock.py
+++ lock.py
@@ -1,14 +1,29 @@
 #!env/bin/python
 import sys
+import random
 
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
 
+# Special value to specify myself as master
+class ME: pass
+
 class LockProtocol(LineReceiver):
+    def __init__(self):
+        self.other_side = (None, None)
+
+
     def connectionMade(self):
         self.sendLine('hello %s %s' % self.factory.address)
+
+
+    def connectionLost(self, reason):
+        print 'Connection to the %s:%s lost.' % self.other_side
+
+        if self.factory.master is None:
+            self._add_election_result('error')
 
 
     def lineReceived(self, line):
@@ -26,31 +41,97 @@ class LockProtocol(LineReceiver):
 
     def sendLine(self, line):
         print 'SEND:', line
-        LineReceiver.sendLine(self, line)
+        return LineReceiver.sendLine(self, line)
 
 
     def cmd_hello(self, host, port):
         print 'Received hello from %s:%s' % (host, port)
 
         port = int(port)
-        if self.factory.connections[(host, port)] is None:
-            print 'Adding %s:%s to the connections list' % (host, port)
-            self.factory.connections[(host, port)] = self
+        addr = (host, port)
+        if self.factory.connections[addr] is None:
+            print 'Adding %s:%s to the connections list' % addr
+            self.factory.connections[addr] = self
+            self.other_side = addr
+            self._check_if_master_should_be_elected()
         else:
-            print 'We already have connection with %s:%s' % (host, port)
+            print 'We already have connection with %s:%s' % addr
             self.transport.loseConnection()
+
+
+    def _check_if_master_should_be_elected(self):
+        if self.factory.master is None and \
+                all(self.factory.connections.values()):
+            self._start_master_election()
+
+
+    def _start_master_election(self):
+        self.factory.weight = random.randint(0, 100)
+        self.factory.election_results = {}
+
+        for addr, connection in self.factory.connections.items():
+            if connection is not None:
+                self.factory.election_results[addr] = None
+                connection.sendLine('master_election %d' % self.factory.weight)
+
+
+    def _add_election_result(self, result):
+        self.factory.election_results[self.other_side] = result
+        done = len(filter(lambda x: x is None, self.factory.election_results.values())) == 0
+        if done:
+            approved = len(filter(lambda x: x == 'approved', self.factory.election_results.values()))
+            if approved > len(self.factory.election_results) / 2:
+                self._set_master(ME)
+
+
+    def _set_master(self, master):
+        self.factory.master = master
+
+        if master == ME:
+            print 'Horay! I am the MASTER!'
+            for connection in self.factory.connections.values():
+                connection.sendLine('i_am_master')
+        else:
+            print 'New master was elected: %s:%s' % master.other_side
+
+        self.factory.election_results = None
+
+
+    def cmd_master_election(self, weight):
+        if self.factory.weight is None:
+            self._start_master_election()
+
+        if int(weight) > self.factory.weight:
+            #import pdb;pdb.set_trace()
+            result = self.sendLine('master_approve')
+        else:
+            #kimport pdb;pdb.set_trace()
+            result = self.sendLine('master_decline')
+
+
+    def cmd_master_approve(self):
+        self._add_election_result('approved')
+
+
+    def cmd_master_decline(self):
+        self._add_election_result('declined')
+
+
+    def cmd_i_am_master(self):
+        self._set_master(self)
 
 
 
 class LockFactory(ClientFactory):
     protocol = LockProtocol
 
-    def __init__(self, port):
+    def __init__(self, port, server_list):
         self.port = port
         self.address = ('localhost', port)
         self.master = None
+        self.weight = None
 
-        self.connections = dict((('localhost', 2000 + x), None) for x in xrange(2))
+        self.connections = dict((addr, None) for addr in server_list)
         del self.connections[('localhost', self.port)]
 
     def startFactory(self):
@@ -85,9 +166,12 @@ class LockFactory(ClientFactory):
 
 
 
-def main(port):
+def main(port, num_servers):
     # Next lines are magic:
-    factory = LockFactory(port)
+    factory = LockFactory(
+        port,
+        [('localhost', 2000 + x) for x in xrange(num_servers)]
+    )
     #factory.protocol = LockProtocol
 
     # 8007 is the port you want to run under. Choose something >1024
@@ -97,7 +181,8 @@ def main(port):
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
-    main(port)
+    num_servers = int(sys.argv[2])
+    main(port, num_servers)
 
 
 
