3e82761d36af82ba8ffcb20e51a8905ee0622d54
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-11-26 14:02:46 +0300

    Long call test.

--- lock/web.py
+++ lock/web.py
@@ -15,6 +15,12 @@ from . exceptions import KeyAlreadyExists, KeyNotFound
 def _get_key(path):
     return path[1:]
 
+
+def long_call(secs):
+    def cb():
+        return 'Long Call Result'
+    return deferLater(reactor, secs, cb)
+
 class Root(resource.Resource):
     isLeaf = True
 
@@ -24,17 +30,10 @@ class Root(resource.Resource):
 
 
     def render_GET(self, request):
-        def cb1(request):
+        def cb1(result):
             print 'cb1 called: %s' % time()
-            return request, ['CALLBACK 1']
-
-        def cb2(result):
-            request, result = result
-            print 'cb2 called: %s' % time()
-            result.append('CALLBACK 2')
-            request.write('\n'.join(result))
-
             a = 10/0
+            request.write(result)
             request.finish()
 
         def eb1(failure):
@@ -43,25 +42,19 @@ class Root(resource.Resource):
             if failure.type == CancelledError:
                 print 'Cancelled'
             else:
-                request.write('ERROR 1 \n')
-                return failure
-
-        def eb2(failure):
-            err(failure, 'Handled in eb2')
-            print 'eb2 called: %s' % time()
-            request.write('ERROR 2\n')
-            request.finish()
+                request.write('ERROR 1\n')
+                request.finish()
 
         def eb3(failure, d):
             print 'eb3 called: %s' % time()
             d.cancel()
             err(failure, 'Test was interrupted')
 
-        d = deferLater(reactor, 1, lambda: request)
+        secs = int(request.args.get('sec', [1])[0])
+
+        d = long_call(secs)
         d.addCallback(cb1)
-        d.addCallback(cb2)
         d.addErrback(eb1)
-        d.addErrback(eb2)
         request.notifyFinish().addErrback(eb3, d)
         return NOT_DONE_YET
 
