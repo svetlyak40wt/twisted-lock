b8b24c5568dc6a32f831b7a1561d70c3ad8b56ca
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-03-18 10:41:43 +0300

    Growl notifications from test.

--- run-unittest-unless-failed.sh
+++ run-unittest-unless-failed.sh
@@ -4,6 +4,10 @@ MAX_TRIES=100
 LOG_FILENAME=unittest.log
 TEST_NAME=$1
 
+function growl()
+{
+    ssh -p 8888 localhost ~/usr/bin/growl "$1"
+}
 
 for ITER in `seq $MAX_TRIES`
 do
@@ -15,10 +19,18 @@ do
     echo "EXIT CODE: $CODE"
 
     if [ $CODE != 0 ]; then
-        echo "Done with exit code=$CODE, iter=$ITER"
+        MSG="Done with exit code=$CODE, iter=$ITER"
+        echo $MSG
+        growl "$MSG"
         break
     fi
 
     echo 'Retrying'
 done
+
+if [ $ITER == $MAX_TRIES ]; then
+    MSG="No more tries left."
+    echo $MSG
+    growl "$MSG"
+fi
 
